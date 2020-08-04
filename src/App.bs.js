20709ec12310f194d73e93c503c2725cee9f6d6d
DanielSuazoHU <daniel.suazo@herounit.io>
2020-08-04 13:45:27 -0600

    work/play switch

--- src/App.bs.js
+++ src/App.bs.js
@@ -4,10 +4,12 @@ var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 var State$MyReactApp = require("./State.bs.js");
 var Timer$MyReactApp = require("./Timer.bs.js");
+var EditTime$MyReactApp = require("./EditTime.bs.js");
 
 function App(Props) {
   var match = React.useReducer(State$MyReactApp.reducer, State$MyReactApp.initialState);
   var dispatch = match[1];
+  var state = match[0];
   React.useEffect((function () {
           var timer = setInterval((function (param) {
                   return Curry._1(dispatch, /* Tick */3);
@@ -17,8 +19,12 @@ function App(Props) {
                     
                   });
         }), []);
-  return React.createElement("div", undefined, React.createElement(Timer$MyReactApp.make, {
-                  seconds: match[0].seconds
+  return React.createElement("div", undefined, React.createElement("button", {
+                  onClick: (function (param) {
+                      return Curry._1(dispatch, /* TogglePhase */4);
+                    })
+                }, "Switch timer"), React.createElement(Timer$MyReactApp.make, {
+                  seconds: state.seconds
                 }), React.createElement("button", {
                   onClick: (function (param) {
                       return Curry._1(dispatch, /* Stop */1);
@@ -31,7 +37,25 @@ function App(Props) {
                   onClick: (function (param) {
                       return Curry._1(dispatch, /* Reset */2);
                     })
-                }, "Reset"));
+                }, "Reset"), React.createElement(EditTime$MyReactApp.make, {
+                  phase: "Work",
+                  value: state.workTime,
+                  onChange: (function (e) {
+                      return Curry._1(dispatch, /* SetTime */{
+                                  _0: /* Work */0,
+                                  _1: e
+                                });
+                    })
+                }), React.createElement(EditTime$MyReactApp.make, {
+                  phase: "Play",
+                  value: state.playTime,
+                  onChange: (function (e) {
+                      return Curry._1(dispatch, /* SetTime */{
+                                  _0: /* Play */1,
+                                  _1: e
+                                });
+                    })
+                }));
 }
 
 var make = App;
--- src/App.re
+++ src/App.re
@@ -10,10 +10,23 @@ let make = () => {
   });
 
   <div>
+    <button onClick={_ => dispatch(TogglePhase)}>
+      {React.string("Switch timer")}
+    </button>
     <Timer seconds={state.seconds} />
     <button onClick={_ => dispatch(Stop)} > {React.string("Stop")} </button>
     <button onClick={_ => dispatch(Start)}> {React.string("Start")} </button>
     <button onClick={_ => dispatch(Reset)}> {React.string("Reset")} </button>
+    <EditTime 
+      phase="Work" 
+      value={state.workTime}
+      onChange={e => dispatch(SetTime(Work, e))}
+    />
+    <EditTime 
+      phase="Play" 
+      value={state.playTime}
+      onChange={e => dispatch(SetTime(Play, e))}
+    />
   </div>
 
 }
